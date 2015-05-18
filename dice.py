6e47b9cf0effc6062ee5ea62c51159a8b49413ac
Xenomorff <brandonjenks32400@gmail.com>
2015-05-18 16:44:19 -0400

    save

--- dice.py
+++ dice.py
@@ -13,12 +13,22 @@ def roll(number=1,sides=6):
     for count in range(number):
         total += rolldie(sides)
     return total
-    
+
+def parse_roll(text):
+    (howmany, sides) = text.split('d')
+    return roll(int(howmany), int(sides)) 
+
+def parse()
+    (howmany, sides) = parse(text)
+    return roll(howmany, sides)
 
 if __name__ =='__main__':
-    for count in range(4):    
+    print(parse_roll('2d10'))
+    exit()
+
+    for count in range(4):  
         hi = rolldie(6)
-        print('roll die')
+        print(c.red +'roll die')
         print(hi)
         bye = roll(3,6)
         print('roll')
