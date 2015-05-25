3d90a241b63f4406357ccfb98e7b5ef14c0d29bb
Xenomorff <brandonjenks32400@gmail.com>
2015-05-25 17:13:42 -0400

    save

--- dice.py
+++ dice.py
@@ -1,5 +1,5 @@
 """
-hello
+rolling
 """
 import random
 import colors as c
@@ -10,31 +10,28 @@ def rolldie(sides=6):
 
 def roll(number=1,sides=6):
     total = 0
-    for count in range(number):
+    for count in range(sides):
         total += rolldie(sides)
     return total
 
-def parse_roll(text):
+def parse(text):
     (howmany, sides) = text.split('d')
-    return roll(int(howmany), int(sides)) 
+    return (int(howmany), int(sides)) 
 
-def parse()
+def parse_roll(text):
     (howmany, sides) = parse(text)
     return roll(howmany, sides)
 
 if __name__ =='__main__':
-    print(parse_roll('2d10'))
-    exit()
-
-    for count in range(4):  
+   # print(parse_roll('2d6'))
+   # exit()
+    for count in range(2):  
         hi = rolldie(6)
-        print(c.red +'roll die')
+        print(c.red +'++roll die++')
         print(hi)
+        print(c.orange+'------')
         bye = roll(3,6)
-        print('roll')
+        print(c.blue+'<<roll>>')
         print(bye)
-
-    
-
-
+        print(c.orange+'------')
 
---
+++ player.py
@@ -0,0 +1,37 @@
+"""A simple player class with random stat gen"""
+
+import colors as c
+from dice import roll
+import random
+import time
+
+class Player():
+
+    def __init__(self):
+        self.strength = roll('3,6')
+        self.vitality = roll('3,6')
+        self.speed = roll('3,6')
+        self.speech = roll('3,6')
+        self.wisdom = roll('3,6')
+    
+    def show_stats(self):
+        text = '''
+        Strength: {s.strength:>2}
+        Vitality: {s.vitality:>2}
+        Speed: {s.speed:>2}
+        Speech: {s.speech:>2}
+        Wisdom: {s.wisdom:>2}
+        '''
+        print(text.format(s=self))
+        
+        
+if __name__ == '__main__':
+    for count in range(5):
+        time.sleep(1)
+        print(c.clear)
+        player = Player()
+        player.show_stats()
+        print(Player)
+
+
+
